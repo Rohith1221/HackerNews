aa266d65ecae4b7eb236d3300babbaba790d4188
Rohith <marganirohith2000@gmail.com>
2022-02-13 20:04:23 +0530

    completed 2nd task and responsive

--- src/components/NameCard/NameCard.jsx
+++ src/components/NameCard/NameCard.jsx
@@ -1,12 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import SecondCard from "../SecondCard/SecondCard";
 import "./NameCard.css";
 function NameCard({ data }) {
+  const [objId, setObjId] = useState("");
+  const [clicked, setClicked] = useState(false);
+
+  const [info, setInfo] = useState([]);
+  const [children, setChildren] = useState([]);
+  const url_2 = "https://hn.algolia.com/api/v1/items/";
+
+  const getData = (e) => {
+    fetch(url_2 + `${e}`)
+      .then((response) => response.json())
+      .then((json) => {
+        setInfo(json);
+        setChildren(json.children);
+        console.log(json);
+      });
+  };
+
   return (
     <div className="namecard-container">
+      {clicked == true && (
+        <SecondCard id={objId} info={info} child={children} />
+      )}
       <ul className="nc-author">
         {data.map((item, index) => {
           return (
-            <li key={index} className="nc-li">
+            <li
+              key={index}
+              className="nc-li"
+              onClick={(e) => {
+                setObjId(item.objectID);
+                setClicked(true);
+                getData(item.objectID);
+                window.scrollTo({ top: 0, behavior: "smooth", transition: 5 });
+              }}
+            >
               <b>Author: </b>
               {item.author}
               <br></br>
---
+++ src/components/SecondCard/SecondCard.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+import "./SecondCard.css";
+function SecondCard({ id, info, child }) {
+  return (
+    <div className="sc-container">
+      <ul>
+        <li className="sc-li">
+          <h2>More details about the article by {info.author}:</h2>
+          <b>Author :</b>
+          {info.author}
+          <br></br>
+          <b>Title: </b>
+          {info.title == null ? "TITLE NOT FOUND" : info.title}
+          <br></br>
+          <b>Points: </b>
+          {info.points == null ? "POINTS NOT FOUND" : info.points}
+          <br></br>
+          <b>Article Link</b>
+          <a href={info.url == null ? "URL NOT FOUND" : info.url}>Click here</a>
+
+          {child.map((item, index) => {
+            return (
+              <li className="sc-comment" key={index}>
+                <b>
+                  Comment By{" "}
+                  {item.author == null ? "AUTHOR NOT FOUND" : item.author} :
+                </b>
+                {item.text}
+              </li>
+            );
+          })}
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default SecondCard;
